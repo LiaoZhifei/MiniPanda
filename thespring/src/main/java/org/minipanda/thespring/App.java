931ca666255d4c81765794c9949f91622ffccc00
2562003564@qq.com <2562003564@qq.com>
2017-09-09 16:25:14 +0800

    由于不会使用本地代码库回滚，只能同步掉远程服务文件，然后再重新修改本地代码库文件并提交

---
+++ thespring/src/main/java/org/minipanda/thespring/App.java
@@ -0,0 +1,15 @@
+package org.minipanda.thespring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("123123");
+    }
+}
