4cc077d968c18e290d227d6459088cf2dd72ee5b
2562003564@qq.com <2562003564@qq.com>
2017-10-04 20:46:31 +0800

    由于@ComponentScan注解的特性，可以随意选择要扫描的包路径，所以将配置文件单独放在一个包下
    Signed-off-by: 2562003564@qq.com <2562003564@qq.com>

--- thespring/src/test/java/org/minipanda/thespring/inaction/ch1/config/CDPlayerConfig.java
+++ thespring/src/test/java/org/minipanda/thespring/inaction/ch1/config/CDPlayerConfig.java
@@ -1,10 +1,10 @@
-package org.minipanda.thespring.inaction.ch1;
+package org.minipanda.thespring.inaction.ch1.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = {"org.minipanda.thespring.inaction.ch1"})
 public interface CDPlayerConfig {
 
 }
